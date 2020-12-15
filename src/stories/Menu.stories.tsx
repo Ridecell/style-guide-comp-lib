b2ed61bd5b77be0c25b55d88786560d234d9aeb8
Isaac Granillo <isaac@ridecell.com>
2020-12-15 13:17:25 -0800

    use menuItemsData

--- src/stories/Menu.stories.tsx
+++ src/stories/Menu.stories.tsx
@@ -4,6 +4,7 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 
 import RCMenu from '../components/Menu'
 import { RCCompProps } from '../components/Menu'
+import menuItemsData from '../data/menu'
 
 import Dashboard from '@material-ui/icons/Dashboard';
 import DataUsage from '@material-ui/icons/DataUsage';
@@ -18,37 +19,5 @@ const Template: Story<RCCompProps> = (args) => <RCMenu {...args} />;
 export const MainMenu = Template.bind({});
 MainMenu.args = {
   selectedTabIndex: 0,
-  menuItemsData: [
-    {
-      id: "dashboard",
-      name: "Dashboard",
-      icon: <Dashboard />,
-    },
-    {
-      id: "activity",
-      name: "Activity",
-      icon: <DataUsage />,
-      tabMenuItems: [
-        {
-          isHeading: true,
-          id: "jobs",
-          name: "Jobs",
-        },
-        {
-          isHeading: false,
-          isDisabled: false,
-          id: "activejobs",
-          name: "Active Jobs",
-          route: "https://material-ui.com/components/menus/",
-          addItemRoute: "https://material-ui.com/components/material-icons/"
-        },
-        {
-          isHeading: false,
-          isDisabled: true,
-          id: "pastjobs",
-          name: "Past Jobs",
-        }
-      ]
-    }
-  ]
+  menuItemsData: menuItemsData
 };
