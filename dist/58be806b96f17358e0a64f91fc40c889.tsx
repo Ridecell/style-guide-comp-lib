2d9ad733afcc0c0eaf9fa0780fd7a1b608a8b143
Isaac Granillo <isaac@ridecell.com>
2020-12-15 18:46:54 -0800

    webpack setup for typescript

---
+++ dist/58be806b96f17358e0a64f91fc40c889.tsx
@@ -0,0 +1,4 @@
+export Button from './components/Button'
+export Label from './components/Label'
+export Menu from './components/Menu'
+export TextField from './components/TextField'
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,7 @@
-import reportWebVitals from './reportWebVitals';
-
 import Button from './components/Button'
 import Label from './components/Label'
 import Menu from './components/Menu'
 import TextField from './components/TextField'
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
 
 export {
   Button,
